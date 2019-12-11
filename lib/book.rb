075f46143667d20e020e6426c0526a8836ea6fc3
John Louis Rumingan <jmrumingan@gmail.com>
2019-12-11 14:56:25 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Book 
 
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title
+
+    def initialize(title = "none", author = "none", page_count=0, genre = "none")
+        @title = title 
+        @author = author
+        @page_count = page_count 
+        @genre = genre 
+    end 
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end 
+end 
 
